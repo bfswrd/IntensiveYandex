56fe63d1381bb8ed6b7fae9a7465bc36e01b1489
bfswrd <stepandorhin@yandex.com>
2022-10-30 14:06:50 +0300

    Update validators:validate_must_be_param

--- Core/validators.py
+++ Core/validators.py
@@ -1,18 +1,20 @@
+import re
+
 from django.core.exceptions import ValidationError
 from functools import wraps
 
 
 def validate_must_be_param(*args):
-    must_be_in_our_item = {i.lower() for i in args}
+    must_be_in_our_item = args
+    pattern = re.compile('^[a-z]*')
 
     @wraps(validate_must_be_param)
     def validate_amazing(value: str):
-        cleaner_value = set(value.lower().split())
+        cleaner_value = "".join(pattern.findall(value))
 
-        difference = must_be_in_our_item - cleaner_value
-
-        if len(difference) == len(must_be_in_our_item):
-            raise ValidationError(f"Обязательно используйте слова {must_be_in_our_item}")
+        for word in must_be_in_our_item:
+            if word in cleaner_value:
+                raise ValidationError(f"Обязательно используйте слова {must_be_in_our_item}")
         return value
 
     return validate_amazing
