391e6b2d1ae78cb204c6d90e564de5d4620dea45
bfswrd <stepandorhin@yandex.com>
2022-10-30 12:16:27 +0300

    Create catalog models and migrations

--- catalog/models.py
+++ catalog/models.py
@@ -1,1 +1,30 @@
-# Create your models here.
+from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+from Core.models import CommonCatalog, Slug
+from Core.validators import validate_must_be_param
+
+
+class Category(CommonCatalog, Slug):
+    weight = models.IntegerField(
+        default=100,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(32766),
+        ],
+    )
+
+
+class Tag(CommonCatalog, Slug):
+    pass
+
+
+class Item(CommonCatalog):
+    text = models.TextField(
+        validators=[
+            validate_must_be_param("превосходно", "прекрасно"),
+        ],
+    )
+
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name="tag")
