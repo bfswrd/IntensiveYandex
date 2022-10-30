4a08437b917c7700e74d5db75c17d5d642a5887c
bfswrd <stepandorhin@yandex.com>
2022-10-30 19:15:01 +0300

    Update validators:validate_must_be_param

--- Core/validators.py
+++ Core/validators.py
@@ -6,14 +6,12 @@ from functools import wraps
 
 def validate_must_be_param(*args):
     must_be_in_our_item = args
-    pattern = re.compile('^[a-z]*')
 
     @wraps(validate_must_be_param)
     def validate_amazing(value: str):
-        cleaner_value = "".join(pattern.findall(value))
 
         for word in must_be_in_our_item:
-            if word in cleaner_value:
+            if word in value:
                 raise ValidationError(f"Обязательно используйте слова {must_be_in_our_item}")
         return value
 
