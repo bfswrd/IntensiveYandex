1efc16b4db534df628a6ca72f7388ca85ad6ae72
bfswrd <stepandorhin@yandex.com>
2022-10-30 11:26:20 +0300

    Create abstract models

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class CommonCatalog(models.Model):
+    name = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=True)
+
+    class Meta:
+        abstract = True
+
+
+class Slug(models.Model):
+    slug = models.SlugField(max_length=200, unique=True)
+
+    class Meta:
+        abstract = True
