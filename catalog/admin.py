47c38c752a76be28362c605262e42506c77ab778
bfswrd <stepandorhin@yandex.com>
2022-10-30 12:17:02 +0300

    Add catalog models in admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,7 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Item, Category, Tag
+
+admin.site.register(Item)
+admin.site.register(Category)
+admin.site.register(Tag)
