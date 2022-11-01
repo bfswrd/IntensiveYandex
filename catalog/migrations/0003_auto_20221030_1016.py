749a17ed44ba46eae672de95ffcb2d848f81edb0
bfswrd <stepandorhin@yandex.com>
2022-11-01 16:37:43 +0300

    Create migrations

--- catalog/migrations/0003_auto_20221030_1016.py
+++ catalog/migrations/0003_auto_20221030_1016.py
@@ -29,12 +29,12 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='is_published',
-            field=models.BooleanField(default=True, help_text='Это поле отвечает за публикацию', verbose_name='Опубликовано'),
+            field=models.BooleanField(default=True, help_text='Введите публикацию', verbose_name='Опубликовано'),
         ),
         migrations.AlterField(
             model_name='category',
             name='name',
-            field=models.CharField(help_text='Это поле отвечает за название', max_length=150, verbose_name='Название'),
+            field=models.CharField(help_text='Введите название', max_length=150, verbose_name='Название'),
         ),
         migrations.AlterField(
             model_name='category',
@@ -44,42 +44,42 @@ class Migration(migrations.Migration):
         migrations.AlterField(
             model_name='category',
             name='weight',
-            field=models.IntegerField(default=100, help_text='Это поле отвечает за вес товара', validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(32766)], verbose_name='Вес'),
+            field=models.IntegerField(default=100, help_text='Введите вес товара', validators=[django.core.validators.MinValueValidator(1), django.core.validators.MaxValueValidator(32766)], verbose_name='Вес'),
         ),
         migrations.AlterField(
             model_name='item',
             name='category',
-            field=models.ForeignKey(help_text='Это поле отвечает за категорию товара', on_delete=django.db.models.deletion.CASCADE, to='catalog.category', verbose_name='Категория'),
+            field=models.ForeignKey(help_text='Введите категорию товара', on_delete=django.db.models.deletion.CASCADE, to='catalog.category', verbose_name='Категория'),
         ),
         migrations.AlterField(
             model_name='item',
             name='is_published',
-            field=models.BooleanField(default=True, help_text='Это поле отвечает за публикацию', verbose_name='Опубликовано'),
+            field=models.BooleanField(default=True, help_text='Введите публикацию', verbose_name='Опубликовано'),
         ),
         migrations.AlterField(
             model_name='item',
             name='name',
-            field=models.CharField(help_text='Это поле отвечает за название', max_length=150, verbose_name='Название'),
+            field=models.CharField(help_text='Введите название', max_length=150, verbose_name='Название'),
         ),
         migrations.AlterField(
             model_name='item',
             name='tags',
-            field=models.ManyToManyField(help_text='Это поле отвечает за теги товара, их может быть очень много', related_name='tag', to='catalog.Tag', verbose_name='Теги'),
+            field=models.ManyToManyField(help_text='Введите теги товара, их может быть очень много', related_name='tag', to='catalog.Tag', verbose_name='Теги'),
         ),
         migrations.AlterField(
             model_name='item',
             name='text',
-            field=models.TextField(help_text='Это поле отвечает за описание товара', validators=[Core.validators.validate_must_be_param], verbose_name='Описание'),
+            field=models.TextField(help_text='Введите описание товара', validators=[Core.validators.validate_must_be_param], verbose_name='Описание'),
         ),
         migrations.AlterField(
             model_name='tag',
             name='is_published',
-            field=models.BooleanField(default=True, help_text='Это поле отвечает за публикацию', verbose_name='Опубликовано'),
+            field=models.BooleanField(default=True, help_text='Введите публикацию', verbose_name='Опубликовано'),
         ),
         migrations.AlterField(
             model_name='tag',
             name='name',
-            field=models.CharField(help_text='Это поле отвечает за название', max_length=150, verbose_name='Название'),
+            field=models.CharField(help_text='Введите название', max_length=150, verbose_name='Название'),
         ),
         migrations.AlterField(
             model_name='tag',
