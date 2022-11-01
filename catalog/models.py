280dc00a1ef86e2dacfe79b6fc8b1596a3b63277
bfswrd <stepandorhin@yandex.com>
2022-11-01 16:46:09 +0300

    Update imports catalog:models

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
 
 from Core.models import CommonCatalog, Slug
 from Core.validators import validate_must_be_param
