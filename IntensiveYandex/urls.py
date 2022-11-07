be2259272920539a3a4812ba1a10aadab1822e08
Stepan <stepandorhin@yandex.ru>
2022-11-07 21:37:31 +0300

    Update base_dir/urls

--- IntensiveYandex/urls.py
+++ IntensiveYandex/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -7,3 +9,4 @@ urlpatterns = [
     path("about/", include("about.urls")),
     path("catalog/", include("catalog.urls"))
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
