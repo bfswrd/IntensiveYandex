20bd7c5eb1bf97bfd05eeb2246ba817e8f61a456
Stepan <stepandorhin@yandex.ru>
2022-11-07 17:36:23 +0300

    Update settings

--- IntensiveYandex/settings.py
+++ IntensiveYandex/settings.py
@@ -85,6 +85,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = "static"
+STATICFILES_DIRS = [
+    BASE_DIR / "static_dev",
+]
+
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
