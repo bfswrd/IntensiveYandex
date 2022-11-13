49a6f6fb662ef591f23cd071d95c2ea4b9f55f4b
Stepan <stepandorhin@yandex.ru>
2022-11-13 16:36:20 +0300

    Update urls.py item-detail

--- catalog/urls.py
+++ catalog/urls.py
@@ -7,5 +7,6 @@ app_name = "catalog"
 urlpatterns = [
     path("", catalog.views.item_list, name="item_list"),
     # Start with 1, 0 is incorrect
-    re_path(r"(?P<pk>^[1-9]\d*)/$", catalog.views.item_detail, name="item_detail"),
+    re_path(r"^(?P<pk>[1-9]\d*)/$", catalog.views.item_detail,
+            name="item_detail"),
 ]
