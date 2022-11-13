c811f57f05fbbef489278ff7c65c034e8e790916
Stepan <stepandorhin@yandex.ru>
2022-11-13 18:16:42 +0300

    Update view catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from catalog.models import Item
+
 
 def item_list(request):
-    return render(request, "catalog/items_list.html")
+    items = Item.objects.published().order_by("category")
+
+    context = {
+        "items": items,
+    }
+    return render(request, "catalog/items_list.html", context=context)
 
 
 def item_detail(request, pk: int):
