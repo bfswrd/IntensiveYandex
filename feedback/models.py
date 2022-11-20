4cc51b1169b5efc0737bea7ccbc1a27a32300d23
Stepan <stepandorhin@yandex.ru>
2022-11-20 12:00:55 +0300

    Update feedback/models

--- feedback/models.py
+++ feedback/models.py
@@ -4,9 +4,10 @@ from django.db import models
 class Feedback(models.Model):
     text = models.TextField(
         verbose_name="Обратная связь",
-        help_text=f"Введите обращение"
+        help_text=f"Введите обращение",
     )
+
     created_on = models.DateTimeField(
         auto_now_add=True, blank=True,
-        verbose_name="Время создания"
+        verbose_name="Время создания",
     )
