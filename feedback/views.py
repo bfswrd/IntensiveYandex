339435f6fda0fa28ce9a254207e8c4b13c030d4e
Stepan <stepandorhin@yandex.ru>
2022-11-20 12:02:30 +0300

    Create feedback/views

--- feedback/views.py
+++ feedback/views.py
@@ -1,2 +1,24 @@
-from django.shortcuts import render
+from django.core.mail import send_mail
+from django.shortcuts import redirect, render
 
+from feedback import forms
+from feedback.models import Feedback
+
+
+def feedback(request):
+    form = forms.FeedbackForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if request.POST and form.is_valid():
+        send_mail(
+            "Привет",
+            (f"Мы улышали тебя и хоти сказать спасибо"
+             f" за обраную свзязь {form.cleaned_data.get('text')}"),
+            "from@to.ru",
+            ["ex1@amp.le", "ex2@amp.le", ],
+            fail_silently=False,
+        )
+        Feedback.objects.create(**form.cleaned_data).save()
+        return redirect("feedback:feedback")
+    return render(request, "feedback/feedback.html", context=context)
