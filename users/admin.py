666bfe96d3e9b66c7e7466dbb6060808bf27c7a7
Stepan <stepandorhin@yandex.ru>
2022-11-28 17:37:26 +0300

    Update users/admin

--- users/admin.py
+++ users/admin.py
@@ -1,18 +1,33 @@
 from django.contrib import admin
-from users.models import User, Birthday
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.admin import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-
-class BirthdayInline(admin.StackedInline):
-    model = Birthday
+from users.forms import UserChangeForm, UserCreationForm
+from users.models import User
 
 
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    inlines = [
-        BirthdayInline
-    ]
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ("email", "is_staff", "is_superuser",)
+    list_filter = ("is_superuser",)
+    fieldsets = (
+        (None, {"fields": (
+            "email", "password", "birthday",
+            "first_name", "last_name",
+            "is_superuser", "is_active")}),
+    )
+    add_fieldsets = (
+        (None, {"fields": (
+            "email", "password", "birthday",
+            "first_name", "last_name",
+            "is_superuser", "is_active")}),
+    )
+
+    search_fields = ("email",)
+    ordering = ("email",)
+    filter_horizontal = ()
 
 
 admin.site.unregister(Group)
