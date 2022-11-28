ffa15428775584e1f832bd1273e72ff04022e5ea
Stepan <stepandorhin@yandex.ru>
2022-11-28 17:42:59 +0300

    Delete trash

--- feedback/views.py
+++ feedback/views.py
@@ -1,6 +1,6 @@
+from django.conf import settings
 from django.core.mail import send_mail
 from django.shortcuts import redirect, render
-from django.conf import settings
 
 from feedback import forms
 from feedback.models import Feedback
