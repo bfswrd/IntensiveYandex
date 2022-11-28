e9c25326c185d8bd7f3caf0d5a9d2265bd8e0fab
Stepan <stepandorhin@yandex.ru>
2022-11-28 17:37:13 +0300

    Add models, migration

--- users/models.py
+++ users/models.py
@@ -1,16 +1,66 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import (AbstractBaseUser, AbstractUser,
+                                        PermissionsMixin)
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
-class User(AbstractUser):
+class CustomUserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        if not email:
+            raise ValueError(_('The email must be set'))
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self._create_user(email, password, **extra_fields)
+
+    def is_active(self):
+        return self.get_queryset().filter(is_active=True)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    birthday = models.DateField(
+        "День рождения",
+        null=True,
+        blank=True
+    )
+    email = models.EmailField(
+        _("Email address"),
+        unique=True,
+        help_text="Введите почту")
+    first_name = models.CharField(_("first name"), max_length=150, blank=True)
+    last_name = models.CharField(_("last name"), max_length=150, blank=True)
+    is_staff = models.BooleanField(default=False, )
+    is_active = models.BooleanField(default=True, )
+    is_superuser = models.BooleanField(default=False, )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
     class Meta:
         verbose_name = "Пользователь"
         verbose_name_plural = "Пользователи"
 
-
-class Birthday(models.Model):
-    birthday = models.DateTimeField(
-        null=True,
-        verbose_name="День рождения"
-    )
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return "{}".format(self.email)
